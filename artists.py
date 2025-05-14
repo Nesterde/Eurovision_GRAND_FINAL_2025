4974bcbbc1ff40875f0139bc89d391b294ca681f
Nesterde <dominik.nesterowicz@gmail.com>
2025-05-14 18:51:33 +0200

    artist list update

--- artists.py
+++ artists.py
@@ -1,4 +1,5 @@
 artists = [
+    # II półfinał - później należy usunąć wyeliminowanych 
     "🇦🇺 Australia – Go-Jo – Milkshake Man",
     "🇲🇪 Czarnogóra – Nina Žižić – Dobrodošli",
     "🇮🇪 Irlandia – Emmy – Laika Party",
@@ -16,6 +17,7 @@ artists = [
     "🇷🇸 Serbia – Princ – Mila",
     "🇫🇮 Finlandia – Erika Vikman – Ich Komme",
 
+    # Finaliści z I półfinału 
     "🇮🇸 Islandia – Væb – Róa",
     "🇵🇱 Polska – Justyna Steczkowska – Gaja",
     "🇪🇪 Estonia – Tommy Cash – Espresso macchiato",
@@ -23,7 +25,14 @@ artists = [
     "🇸🇪 Szwecja – KAJ – Bara bada bastu",
     "🇵🇹 Portugalia – Nena – Deslocado",
     "🇳🇴 Norwegia – Kyle Alessandro – Lighter",
-    "🇸🇲 San Marino – Megara – Tutta l'Italia",
+    "🇸🇲 San Marino – Gabry Ponte – Tutta l'Italia",
     "🇦🇱 Albania – Besa – Zjerm",
-    "🇳🇱 Niderlandy – Claude – C'est La Vie"
+    "🇳🇱 Niderlandy – Claude – C'est La Vie",
+
+    # Wielka Piątka
+    "🇮🇹 Włochy – Lucio Corsi – Volevo essere un duro",
+    "🇩🇪 Niemcy – Abor & Tynna – Baller",
+    "🇫🇷 Francja – Louane – Maman",
+    "🇪🇸 Hiszpania – Melody – Esa diva",
+    "🇬🇧 Wielka Brytania – Remember Monday – What the Hell Just Happened?"
 ]
